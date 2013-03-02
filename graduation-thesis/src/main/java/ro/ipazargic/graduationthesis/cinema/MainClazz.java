e46c42335ec935111c905a8cbedebaf262662977
ipazargic <irina.pazargic@gmail.com>
2013-03-02 20:52:40 +0200

    Irina println

--- graduation-thesis/src/main/java/ro/ipazargic/graduationthesis/cinema/MainClazz.java
+++ graduation-thesis/src/main/java/ro/ipazargic/graduationthesis/cinema/MainClazz.java
@@ -1,18 +1,19 @@
 package ro.ipazargic.graduationthesis.cinema;
 
 public class MainClazz {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		MainClazz mc = new MainClazz();
-		mc.print();
-	}
-
-	public void print() {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        MainClazz mc = new MainClazz();
+        mc.print();
+    }
+    
+    public void print() {
         System.out.println("From jtonic.");
+        System.out.println("Irina");
         System.out.println("Hello java world!!!");
-	}
-	
+    }
+    
 }
